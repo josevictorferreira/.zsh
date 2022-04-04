2d9065aa82b2932e858c2735a82beb9b6856775d
Jose Victor <josevictorferreira@outlook.com>
2022-04-03 22:21:48 -0300

    added update_neovim function

--- functions.zsh
+++ functions.zsh
@@ -1,8 +1,15 @@
 
+## Automatically creates and runs a phoenix livebook container
 run_livebook () {
   docker run -p 8080:8080 --pull always -u $(id -u):$(id -g) -v $(pwd):/data livebook/livebook
 }
 
+## Convert a given text string to base64, automatically copies to clipboard
 function b64() {
   echo -n "$1" | base64 -w 0 | xclip -sel clip
+}
+
+## Automatically updates neovim
+function update_neovim() {
+  asdf plugin update neovim && asdf uninstall neovim nightly && asdf install neovim nightly
 }
